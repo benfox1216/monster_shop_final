d0b343592cbe9603eb6b24ecf4be168b65624400
Ben Fox <benfox1216@gmail.com>
2020-03-14 06:24:42 -0600

    Rewrite routes to not use rails resources

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,35 @@ Rails.application.routes.draw do
   get :root, to: 'welcome#index'
   get '/', to: 'welcome#index'
 
-  resources :merchants do
-    resources :items, only: [:index]
-  end
+  # resources :merchants do
+  #   resources :items, only: [:index]
+  # end
+  
+  get '/merchants', to: 'merchants#index'
+  get '/merchants/new', to: 'merchants#new'
+  post '/merchants', to: 'merchants#create'
+  get '/merchants/:id', to: 'merchants#show'
+  get '/merchants/:id/edit', to: 'merchants#edit'
+  patch '/merchants/:id', to: 'merchants#update'
+  delete '/merchants/:id', to: 'merchants#destroy', as: 'merchant'
+  
+  get '/merchants/:merchant_id/items', to: 'items#index'
 
-  resources :items, only: [:index, :show] do
-    resources :reviews, only: [:new, :create]
-  end
+  # resources :items, only: [:index, :show] do
+  #   resources :reviews, only: [:new, :create]
+  # end
+  
+  get '/items', to: 'items#index'
+  get '/items/:id', to: 'items#show', as: 'item'
+  
+  get '/items/:item_id/reviews/new', to: 'reviews#new', as: 'new_item_review'
+  post '/items/:item_id/reviews', to: 'reviews#create', as: 'item_reviews'
 
-  resources :reviews, only: [:edit, :update, :destroy]
-
+  #resources :reviews, only: [:edit, :update, :destroy]
+  get '/reviews/:id/edit', to: 'reviews#edit', as: 'edit_review'
+  patch '/reviews/:id', to: 'reviews#update'
+  delete '/reviews/:id', to: 'reviews#destroy', as: 'review'
+  
   get '/cart', to: 'cart#show'
   post '/cart/:item_id', to: 'cart#add_item'
   delete '/cart', to: 'cart#empty'
@@ -20,7 +39,11 @@ Rails.application.routes.draw do
   delete '/cart/:item_id', to: 'cart#remove_item'
 
   get '/registration', to: 'users#new', as: :registration
-  resources :users, only: [:create, :update]
+  
+  #resources :users, only: [:create, :update]
+  post '/users', to: 'users#create', as: 'users'
+  patch '/users/:id', to: 'users#update', as: 'user'
+  
   patch '/user/:id', to: 'users#update'
   get '/profile', to: 'users#show'
   get '/profile/edit', to: 'users#edit'
@@ -36,17 +59,44 @@ Rails.application.routes.draw do
 
   namespace :merchant do
     get '/', to: 'dashboard#index', as: :dashboard
-    resources :orders, only: :show
-    resources :items, only: [:index, :new, :create, :edit, :update, :destroy]
-    resources :discounts, only: [:index, :new, :create, :edit, :update, :destroy]
+    
+    #resources :orders, only: :show
+    get '/orders/:id', to: 'orders#show'
+    
+    #resources :items, only: [:index, :new, :create, :edit, :update, :destroy]
+    get '/items', to: 'items#index'
+    get '/items/new', to: 'items#new'
+    post '/items', to: 'items#create'
+    get '/items/:id/edit', to: 'items#edit'
+    put '/items/:id', to: 'items#update'
+    patch '/items/:id', to: 'items#update'
+    delete '/items/:id', to: 'items#destroy'
+    
+    #resources :discounts, only: [:index, :new, :create, :edit, :update, :destroy]
+    get '/discounts', to: 'discounts#index'
+    get '/discounts/new', to: 'discounts#new'
+    post '/discounts', to: 'discounts#create'
+    get '/discounts/:id/edit', to: 'discounts#edit'
+    put '/discounts/:id', to: 'discounts#update'
+    patch '/discounts/:id', to: 'discounts#update'
+    delete '/discounts/:id', to: 'discounts#destroy'
+    
     put '/items/:id/change_status', to: 'items#change_status'
     get '/orders/:id/fulfill/:order_item_id', to: 'orders#fulfill'
   end
 
   namespace :admin do
     get '/', to: 'dashboard#index', as: :dashboard
-    resources :merchants, only: [:show, :update]
-    resources :users, only: [:index, :show]
+    
+    #resources :merchants, only: [:show, :update]
+    get '/merchants/:id', to: 'merchants#show'
+    put '/merchants/:id', to: 'merchants#update'
+    patch '/merchants/:id', to: 'merchants#update'
+    
+    #resources :users, only: [:index, :show]
+    get '/users', to: 'users#index'
+    get '/users/:id', to: 'users#show'
+    
     patch '/orders/:id/ship', to: 'orders#ship'
   end
 end
